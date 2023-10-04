c5291b47aac47ef2f48a658212d0670cc2953f1f
nalugala-vc <vanessachebukwa@gmail.com>
2023-10-04 12:31:43 +0300

    merge with english and generate excel done

--- data_to_xlsx_q1.py
+++ data_to_xlsx_q1.py
@@ -28,3 +28,31 @@ def read_jsonl_file(path: str) -> pd.DataFrame:
     df = pd.read_json(path, lines=True)
     return df
 
+
+def merge_with_english(english_df: pd.DataFrame, other_df: pd.DataFrame) -> pd.DataFrame:
+    df = other_df.set_index('id').join(english_df.set_index('id'), rsuffix='_english')
+    df = df.rename(columns={
+        "utt_english": "utt_translation",
+        "annot_utt_english": "annot_utt_translation",
+        "utt": "utterance",
+        "annot_utt": "annot_utt",
+    })
+    df.reset_index(inplace=True)
+    df = df[["id", "utterance", "utt_translation", "annot_utt_translation", "annot_utt", "partition"]]
+    return df
+
+
+# Logic
+
+def generate_xlsx_file(file: str) -> None:
+    dataset_path = os.path.join(dataset_dir, file)
+    df = read_jsonl_file(dataset_path)
+    lang_code = file.split('-')[1].split('.')[0]
+    english_jsonl = pd.read_json(f"{dataset_dir}/{english_dataset}", lines=True)
+    df = merge_with_english(english_jsonl, df)
+
+    # Ensure processed_files_dir exists before saving
+    if not os.path.exists(processed_files_dir):
+        os.makedirs(processed_files_dir)
+
+    df.to_excel(f'{processed_files_dir}/en-{lang_code}.xlsx')
