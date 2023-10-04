9090becba74845186f6fc0533b98b3faf3edb9de
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-10-04 22:50:43 +0300

    Resolving error: language dir not populating.

--- jsonl_gen_q2.py
+++ jsonl_gen_q2.py
@@ -1,23 +1,23 @@
+"""
+This script filters records from input JSONL files for English (en), Swahili (sw), and German (de) based on their
+partitions, and outputs separate JSONL files for each language and the respective partition
+"""
 import os
 import json
+import logging
 from absl import app
 from flags_config import FLAGS
 
-input_directory = 'amazon-dataset'
-output_directory = 'languages'
-
-os.makedirs(output_directory, exist_ok=True)
-
-languages = set(['en-US', 'sw-KE', 'de-DE'])
-partitions = ['test', 'train', 'dev']
-
-
+logging.basicConfig(level=logging.INFO)
 
 def filter_and_save_records(input_file, language):
-    # Open all the output files for the current language at once
+    """
+    Filters records from an input JSONL file by matching the 'locale' field with a specified language prefix
+    and save them into separate JSONL files based on their 'partition' field.
+    """
     output_files = {
-        partition: open(os.path.join(output_directory, f'{language}-{partition}.jsonl'), 'w', encoding='utf-8')
-        for partition in partitions}
+        partition: open(os.path.join(FLAGS.output_directory, f'{language}-{partition}.jsonl'), 'w', encoding='utf-8')
+        for partition in FLAGS.partitions}
 
     with open(input_file, 'r', encoding='utf-8') as file:
         for line in file:
@@ -26,21 +26,21 @@ def filter_and_save_records(input_file, language):
                 json.dump(record, output_files[record['partition']])
                 output_files[record['partition']].write('\n')
 
-    # Close all the output files for the current language
     for file in output_files.values():
         file.close()
 
-
-for language in languages:
-    input_file = os.path.join(input_directory, f'{language}.jsonl')
-    filter_and_save_records(input_file, language)
-
 def main(argv):
+    """
+    Iterates through the provided languages, reads input JSONL files for each language,
+    filters records using filter_and_save_records() function
+    """
     os.makedirs(FLAGS.output_directory, exist_ok=True)
-
+    logging.info("Generating separate jsonl files...")
     for language in FLAGS.languages:
         input_file = os.path.join(FLAGS.input_directory, f'{language}.jsonl')
         filter_and_save_records(input_file, language)
 
-    print('JSONL files have been generated and saved to', FLAGS.output_directory)
+    logging.info('JSONL files have been generated and saved to %s', FLAGS.output_directory)
 
+if __name__ == "__main__":
+    app.run(main)
