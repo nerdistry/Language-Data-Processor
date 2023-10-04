e1707bfe07d3b212ac275107428161484cf34d21
nalugala-vc <vanessachebukwa@gmail.com>
2023-10-04 12:33:54 +0300

    generator.sh q2 complete

--- generator.sh
+++ generator.sh
@@ -8,3 +8,10 @@ python data_to_xlsx_q1.py \
     --dataset_dir=amazon-dataset \
     --output_dir=processed-dataset
 
+# Run the second Python script
+python jsonl_gen_q2.py \
+    --input_directory=amazon-dataset \
+    --output_directory=languages \
+    --languages=en-US,sw-KE,de-DE \
+    --partitions=test,train,dev
+
