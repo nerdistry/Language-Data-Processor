8a7011dfc16f356f762703ccf875be90a87b5464
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-10-04 13:08:59 +0300

    Removed spacings, weird comments from dataset_extraction.py

--- dataset_extraction.py
+++ dataset_extraction.py
@@ -3,10 +3,8 @@ import sys
 import time
 from absl import app, flags
 
-import pandas as pd
 import tarfile
 import os
-
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('tarfile_path', 'data/amazon_massive_dataset.tar.gz', 'Path to the tar.gz file that needs to be extracted.')
@@ -20,8 +18,6 @@ def extract_dataset(filename: str) -> list:
 
     spinner = itertools.cycle(['-', '/', '|', '\\'])
 
-
-
     with tarfile.open(filename, "r:gz") as tar:
         for member in tar.getmembers():
             sys.stdout.write(next(spinner))
@@ -29,12 +25,10 @@ def extract_dataset(filename: str) -> list:
             time.sleep(0.15)
             sys.stdout.write('\b')
             if member.isfile() and member.name.endswith('.jsonl'):
-                # Modify member's name to just the filename to prevent directory structure replication
                 member.name = os.path.basename(member.name)
                 tar.extract(member, path=destination_dir)
                 extracted_files.append(member.name)
 
     return extracted_files
-
 
 extracted_files = extract_dataset("data/amazon_massive_dataset.tar.gz")
